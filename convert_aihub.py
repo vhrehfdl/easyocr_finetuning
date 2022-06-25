e307e171a179c0ac43fd8520b16b64f00e15ab62
LeeJungHoon <vhrehfdl7tp@naver.com>
2022-06-26 00:09:34 +0900

    update step2 gt.txt

--- convert_aihub.py
+++ convert_aihub.py
@@ -11,21 +11,16 @@ if __name__ == "__main__":
     parser.add_argument('--output_dir')
     opt = parser.parse_args()
 
-    images_list = os.listdir(opt.input_image_dir)
-    label_list = os.listdir(opt.input_label_dir)
-
     with open(opt.output_dir+"gt.txt", "w") as f:
-        for folder_num in images_list:
-            for name in os.listdir(opt.input_label_dir):
-                if name.split(".")[1] == "json":
-                    name = name.split(".")[0]
-
-                    json_path = opt.input_label_dir+name+".json"
-                    with open(json_path, 'r') as file:
-                        data = json.load(file)
-                    
-                    for i, word in enumerate(data["text"]["word"]):
-                        img = Image.open(opt.input_image_dir+name+".jpg")
-                        img_crop = img.crop((word["wordbox"][0], word["wordbox"][1], word["wordbox"][2], word["wordbox"][3]))
-                        img_crop.save(opt.output_dir+"images/"+name+"_"+str(i)+".jpg")
-                        f.write("images/"+name+"_"+str(i)+".jpg\t"+word["value"]+"\n")
+        for name in os.listdir(opt.input_label_dir):
+            if name.split(".")[1] == "json":
+                name = name.split(".")[0]
+                
+                with open(opt.input_label_dir+name+".json", 'r') as file:
+                    data = json.load(file)
+                
+                for i, word in enumerate(data["text"]["word"]):
+                    img = Image.open(opt.input_image_dir+name+".jpg")
+                    img_crop = img.crop((word["wordbox"][0], word["wordbox"][1], word["wordbox"][2], word["wordbox"][3]))
+                    img_crop.save(opt.output_dir+"images/"+name+"_"+str(i)+".jpg")
+                    f.write("images/"+name+"_"+str(i)+".jpg\t"+word["value"]+"\n")
